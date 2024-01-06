4fbd14c17ef4552a430587923488ee5042c0ac4d
Maksym Kultyshev <mksbnt@icloud.com>
2024-01-07 01:02:11 +0200

    refactoring: make sliderService private readonly

--- src/app/core/components/time/time.component.ts
+++ src/app/core/components/time/time.component.ts
@@ -1,20 +1,16 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ControllerSliderService } from '../../services/controller-slider.service';
 
 @Component({
   selector: 'app-time',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <time>
-      {{ sliderService.sliderTimestamp$ | async | date : 'M/d/yy, H:mm:ss' }}
-    </time>
-  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `<time>{{ time$ | async | date : 'M/d/yy, H:mm:ss' }}</time>`,
+  styles: `:host {display: flex; justify-content: center;}`,
 })
 export class TimeComponent {
-  public sliderService: ControllerSliderService = inject(
-    ControllerSliderService
-  );
+  private readonly sliderService = inject(ControllerSliderService);
+  time$ = this.sliderService.sliderTimestamp$;
 }
