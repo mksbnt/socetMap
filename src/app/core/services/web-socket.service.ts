b71852b6ca308cc467d46fe4e45c66d9c113c6f2
Maksym Kultyshev <mksbnt@icloud.com>
2024-01-10 12:04:02 +0200

    Refactor WebSocketService to use NotificationService
    The snack bar for user notifications has been replaced with a more reusable NotificationService in the WebSocketService. Code related to managing a "previous" WebSocket was removed as it seemed unnecessary. The webSocket field was moved lower in the file, but its functionality is unchanged.

--- src/app/core/services/web-socket.service.ts
+++ src/app/core/services/web-socket.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, inject } from '@angular/core';
-import { WebSocketSubject } from 'rxjs/webSocket';
-import { ISignal } from '../interfaces/signal.interface';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import {Injectable, inject} from '@angular/core';
+import {WebSocketSubject} from 'rxjs/webSocket';
+import {ISignal} from '../interfaces/signal.interface';
+import {MatSnackBar} from '@angular/material/snack-bar';
 import {
   BehaviorSubject,
   catchError,
@@ -11,7 +11,8 @@ import {
   tap,
   throwError,
 } from 'rxjs';
-import { createWebSocketSubjectFactory } from '../factories/web-socket.factory';
+import {createWebSocketSubjectFactory} from '../factories/web-socket.factory';
+import {NotificationService} from "./notification.service";
 
 const message = {
   established: 'The websocket connection is established',
@@ -24,30 +25,10 @@ const url = 'ws://localhost:3000';
   providedIn: 'root',
 })
 export class WebSocketService {
-  private _webSocket: WebSocketSubject<ISignal[]> =
-    createWebSocketSubjectFactory(url)();
-  public get webSocket(): WebSocketSubject<ISignal[]> {
-    return this._webSocket;
-  }
-  public set webSocket(value: WebSocketSubject<ISignal[]>) {
-    this._webSocket = value;
-  }
-
-  public connectionStatus$: BehaviorSubject<boolean> =
-    new BehaviorSubject<boolean>(false);
-
-  private snackBar: MatSnackBar = inject(MatSnackBar);
-
+  private notificationService = inject(NotificationService);
+  public connectionStatus$ = new BehaviorSubject<boolean>(false);
   private _isConnected = new BehaviorSubject<boolean>(false);
-  public isConnected$ = this._isConnected.asObservable();
-
-  private _previousWebSocket: WebSocketSubject<ISignal[]> | null = null;
-  public get previousWebSocket(): WebSocketSubject<ISignal[]> | null {
-    return this._previousWebSocket;
-  }
-  public set previousWebSocket(value: WebSocketSubject<ISignal[]> | null) {
-    this._previousWebSocket = value;
-  }
+  // public isConnected$ = this._isConnected.asObservable();
 
   constructor() {
     this._webSocket
@@ -60,7 +41,7 @@ export class WebSocketService {
             () => new Error('WebSocket connection failed', error)
           );
         }),
-        retry({ delay: 5000 }), // Retry with 5-second delay
+        retry({delay: 5000}), // Retry with 5-second delay
         tap(() => {
           this.connectionStatus$.next(true);
         })
@@ -72,16 +53,23 @@ export class WebSocketService {
       .pipe(distinctUntilChanged(), skip(1))
       .subscribe((value) => {
         value
-          ? this.showMessage(message.established)
-          : this.showMessage(message.lost);
+          ? this.showNotification(message.established)
+          : this.showNotification(message.lost);
       });
   }
 
-  showMessage(message: string) {
-    this.snackBar.open(message, 'Close', {
-      duration: 3000,
-      horizontalPosition: 'center',
-      verticalPosition: 'top',
-    });
+  private _webSocket: WebSocketSubject<ISignal[]> =
+    createWebSocketSubjectFactory(url)();
+
+  public get webSocket(): WebSocketSubject<ISignal[]> {
+    return this._webSocket;
+  }
+
+  public set webSocket(value: WebSocketSubject<ISignal[]>) {
+    this._webSocket = value;
+  }
+
+  showNotification(message: string): void {
+    this.notificationService.showNotification(message)
   }
 }
